e208818b77990c0f76f32e0937b14e561dc9c2ba
Gyeongho Yang <chchch1213@naver.com>
2023-09-08 11:55:05 +0900

    Add home widget

---
+++ android/app/src/main/java/com/womosoft/memo/MemoWidget.kt
@@ -0,0 +1,19 @@
+package com.womosoft.memo
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class MemoWidget : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+
+    }
+}
